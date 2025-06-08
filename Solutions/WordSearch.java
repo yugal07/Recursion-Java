0621d8c971b8f16594bbb3dbdd9ced6a7a84bd77
yugal07 <yashsadhwani544@gmail.com>
2025-06-08 19:36:24 +0530

    Refactoring

--- Solutions/WordSearch.java
+++ Solutions/WordSearch.java
@@ -30,29 +30,32 @@
 // board and word consists of only lowercase and uppercase English letters.
 
 public class WordSearch {
-    public static boolean exist(char[][] boards , String string){
-        for(int i = 0 ; i < boards.length ; i++) {
-            for(int j = 0 ; j < boards[0].length ; j++) {
-                if(search(boards, string, i, j, 0)) {
+    public static boolean exists(char [][] board , String string){
+        for(int i = 0 ; i < board.length ; i++) {
+            for(int j = 0 ; j < board[i].length ; j++) {
+                if(search(board, string, i, j, 0)) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-    public static boolean search(char[][] board , String string , int i , int j , int index){
+    public static boolean search(char [][] board , String string , int i , int j , int index){
         if(index == string.length()) {
             return true;
         }
-
-        if(i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != string.charAt(index)) {
+        if(i < 0 || j < 0 || i >= board.length ||  j >= board[0].length || board[i][j] != string.charAt(index)) {
             return false;
         }
 
         char temp = board[i][j];
         board[i][j] = '#';
-        boolean found = search(board, string, i + 1, j, index + 1) || search(board, string, i - 1, j, index + 1) || search(board, string, i, j - 1, index + 1) || search(board, string, i, j + 1, index + 1);
+
+        boolean found = search(board, string, i + 1, j, index + 1) || 
+                        search(board, string, i - 1, j, index + 1) ||
+                        search(board, string, i, j + 1, index + 1) ||
+                        search(board, string, i, j - 1, index + 1);
+
 
         board[i][j] = temp;
         return found;
